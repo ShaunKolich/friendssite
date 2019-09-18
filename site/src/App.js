622978faeb31fd1fe6c4d8168dd7dab560ba94bb
ShaunKolich <employmeinc@gmail.com>
2019-09-18 17:41:46 -0400

    Changing over to a Portfolio Site

--- site/src/App.js
+++ site/src/App.js
@@ -4,44 +4,64 @@ import './App.css';
 import styled from 'styled-components';
 import { Container, Row, Col } from 'fluid-react';
 import Menu from './components/main-nav';
-import Logo from '../src/img/Logo.jpg';
+import Logo from '../src/img/e43e0e8c-3b34-46e3-9154-d94adb1bf0ff_200x200 (1).png';
 import { Button } from 'reactstrap';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './pages/home'
 
 const MainContainer = styled.div`
 width:100%;
-border:2px solid blue;
-height:120px;
+// border:2px solid blue;
+height:100px;
 
 `;
 
 const LogoDiv = styled.div`
 width: 30vh;
 height:100%;
+display:flex;
+justify-content:center;
 
 img {
-  max-width:100%;
+  max-width:150%;
   height:auto;
 }
 `;
 
 const LoginButton = styled.div`
 // border:2px solid red;
-width:300px;
+width:100%;
 height:80px;
 display:flex;
   flex-direction: row;
   justify-content:center;
   align-items:flex-end;
+  outline: none;
+  
 
 Button {
-  width:30%;
+  width:15vh;
   height:50%;
   // display:flex;
   // flex-direction: row;
   // justified-content:center;
+  outline: none!important;
+  color:grey;
+  border-color:grey;
+  
+
 }
+Button:hover{
+  background-color: black;
+  border-color:black;
+  outline: none;
+  
+}
+Button:focus{
+  outline:none;
+}
+
+
 
 `;
 
@@ -65,10 +85,10 @@ function App() {
               </Col>
               <Col>
                 <Row>
-                  <LoginButton>
+                  {/* <LoginButton>
                     <Button outline color="info">Login</Button>{' '}
                     <Button outline color="info">Register</Button>{' '}
-                  </LoginButton>
+                  </LoginButton> */}
                 </Row>
               </Col>
 
--- site/src/pages/home.js
+++ site/src/pages/home.js
@@ -1,29 +1,72 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'fluid-react';
 import styled from 'styled-components';
-import ControlledCarousel from '../components/slider';
+import Slider from '../components/slider';
+import Resume from '../img/Full Stack Web Development.pdf';
+import { Link } from 'react-router-dom';
 
 const SliderWrapper = styled.div`
 width:100%;
 height:500px;
-border:2px solid red;
+// border:2px solid red;
 
 `;
 
-class Home extends Component{
-    render() {
-        return (
-           <div>
-            <SliderWrapper>
-            <ControlledCarousel/>    
+const AboutMe = styled.div`
+border:2px solid red;
+width:100%;
+height:500px;
+display flex;
+justify-content:center;
+flex-direction:column;
+align-content:center;
 
-           </SliderWrapper> 
-           </div>
-                
-           
-    )
+ 
+Button{
+    width:20%;
+    margin-bottom:10px;
+    display:flex;
+    justify-content:center;
+    margin: 0 auto;
+    
+.button{
+    color:white;
+    text-decoration:none;
 
 }
+.button:hover{
+    color:black;
+}
+}
+
+`;
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <SliderWrapper>
+                    <Slider />
+
+                </SliderWrapper>
+                <AboutMe>
+                    <h1>About Me</h1>
+                    <p>Hard-working professional with 13+ years of experience and a proven knowledge of project lifecycle,
+troubleshooting, and desktop technology. Aiming to leverage my skills to successfully fill the Full Stack Web
+Developer role at your company.</p>
+                    <button><Link to= {Resume} className = "button" target="_blank" download>Download</Link></button>
+                    <button><a href="https://www.linkedin.com/in/shaun-kolich-98517450/" className = "button" target="_blank">LinkedIn</a></button>
+                    <button>Check out Github</button>
+                </AboutMe>
+
+
+
+            </div>
+
+
+        )
+
+    }
 
 }
 
